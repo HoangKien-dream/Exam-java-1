ac4732587d992723eef776a6719d8f66a7dc5492
Hoang Kien <hoangkien@gmail.com>
2021-06-29 20:08:44 +0700

    Exam java 1

---
+++ src/demo/exam1/exercies1/ConversionUtil.java
@@ -0,0 +1,10 @@
+package demo.exam1.exercies1;
+
+public class ConversionUtil {
+   public double fahrenheitToCelsius (double fDegree){
+       return  (fDegree-32)/1.8;
+   }
+   public double celciusToFahrenheit(double cDegree){
+       return 1.8 * cDegree+32;
+   }
+}
